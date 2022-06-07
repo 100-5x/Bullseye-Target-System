55f308f647e68627faf60326c5e95758a2fa91a1
bullseye <90105099+100-5x@users.noreply.github.com>
2022-06-07 14:39:25 -0400

    New Stepper Version of System with Tom's phone App

---
+++ esp32wifi_stepper/esp32wifi_stepper.ino
@@ -0,0 +1,93 @@
+ // started with excellent tutorial found here:
+ // https://randomnerdtutorials.com/esp32-access-point-ap-web-server/
+#include <WiFi.h>
+
+#include <AccelStepper.h>
+
+// Define stepper motor connections and motor interface type. Motor interface type must be set to 1 when using a driver:
+// DIR- && PUL- to GND
+#define dirPin 5 // DIR+
+#define stepPin 7 // PUL+
+#define sigPin 9 //From wireless receiver
+#define motorInterfaceType 1
+
+// Create a new instance of the AccelStepper class:
+AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
+
+const char* ssid     = "target.Wifi";
+WiFiServer server(80);
+String header;
+
+// Auxiliar variables to store the current output state
+String activationPinState = "off";
+const int activationPin = 26;
+void setup() {
+  stepper.setMaxSpeed(2000);
+  stepper.setAcceleration(700);
+  pinMode(sigPin, INPUT); 
+  Serial.begin(115200);
+  pinMode(activationPin, OUTPUT);
+  digitalWrite(activationPin, LOW);
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid);//, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  
+  server.begin();
+}
+
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /face") >= 0) {
+              Serial.println("Targets should be facing");
+              stepper.moveTo(90);
+            } else if (header.indexOf("GET /edge") >= 0) {
+              Serial.println("Targets Edge facing");
+              stepper.moveTo(0);
+            }
+           stepper.runToPosition();
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
