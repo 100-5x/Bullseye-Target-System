14a1351a6e67080bb0f2b6f72fb7b339a284cf07
bullseye <90105099+100-5x@users.noreply.github.com>
2022-08-12 18:14:44 -0400

    Stepper update with power timer.

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,12 +1,3 @@
-//#define __DEBUG__
-
-#ifdef __DEBUG__
-   #define print(...)   Serial.print(__VA_ARGS__)
-   #define println(...) Serial.println(__VA_ARGS__)
-#else
-   #define print(...)
-   #define println(...)
-#endif
 
 #include <SpeedyStepper.h>
 #include <WebServer.h>
@@ -17,6 +8,7 @@
 #define dirPin 18 // DIR+
 #define stepPin 19 // PUL+
 #define motorInterfaceType 1
+
 
 const char* ssid = "target.Wifi";
 
@@ -40,13 +32,12 @@ int edgePos = 90;
 //---------------------------------------//
 
 void setup() {
+  delay(1000); // give power time to stabilize.
   // Accel Stepper Variables
   //stepper.setMaxSpeed(1000);
   stepper.connectToPins(stepPin, dirPin);
 
-#ifdef __DEBUG__
   Serial.begin(115200);
-#endif
 
   pinMode(activationPin, OUTPUT);
   digitalWrite(activationPin, LOW);
@@ -62,10 +53,9 @@ void setup() {
   //Serial.println(voltage);
   
 
-  // Set up Wifi
-#ifdef __DEBUG__
   Serial.print("Setting AP (Access Point)…");
-#endif
+
+
   WiFi.mode(WIFI_AP);
   WiFi.softAP("target.Wifi");
   WiFi.softAP(ssid);
@@ -73,50 +63,40 @@ void setup() {
   server.on("/edge", targetEdge);      //Which routine to handle at edge location
   server.on("/face", targetFace);      //Which routine to handle at face location
   server.on("/", handleRoot);      //Which routine to handle at root location
-#ifdef __DEBUG__
   Serial.print("AP IP address: ");
   Serial.println(IP);
-#endif
   server.begin();                  //Start server
 
-#ifdef __DEBUG__
   Serial.println("HTTP server started");
-#endif
+
 }
 
 void loop(){
   server.handleClient();          //Handle client requests
-  stepper.setSpeedInStepsPerSecond(100);
-  stepper.setAccelerationInStepsPerSecondPerSecond(100);
-
-  
+  stepper.setSpeedInStepsPerSecond(200);
+  stepper.setAccelerationInStepsPerSecondPerSecond(200);
   delay(500);
 }
 void handleRoot() {
   server.send(200, "text/plain", "Target system requires http://192.168.4.1/face or http://192.168.4.1/edge");
 }
+
+
 void targetEdge() {
   server.send(200, "text/plain", "Targets Edged");
- 
   digitalWrite(activationPin, HIGH);
   digitalWrite(ledPin, LOW);
-#ifdef __DEBUG__
-   Serial.println("Edged...");
+  Serial.println("Edged...");
   Serial.println(edgePos);
-#endif
-  stepper.moveToPositionInSteps(50);
+  stepper.moveToPositionInSteps(-200);
  
 }
 void targetFace() {
   server.send(200, "text/plain", "Targets Faced");
-
-  
   digitalWrite(activationPin, LOW);
   digitalWrite(ledPin, LOW);
-#ifdef __DEBUG__
   Serial.println("Faced...");
   Serial.println(0);
-#endif
   stepper.moveToPositionInSteps(0);
   
 }
