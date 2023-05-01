77fe13b17c2cb742c68945440ec36d43f28f8720
Bullseye <90105099+100-5x@users.noreply.github.com>
2023-05-01 14:43:07 -0400

    structure change

--- TTS_stand_work/TTS_stand_work.ino
+++ TTS_stand_work/TTS_stand_work.ino
@@ -59,12 +59,12 @@ WebServer server(80); //Server on port 80
 //Must match the sender structure
 //Create a struct_message called myData
 
-typedef struct test_struct {
+typedef struct tts_struct {
   char cmd;
-} test_struct;
+} tts_struct;
 
 //Create a struct_message called myData
-test_struct myData;
+tts_struct myData;
 
 //callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
